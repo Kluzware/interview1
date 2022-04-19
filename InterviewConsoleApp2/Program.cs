d770757564ab69f20da380645080856f22943bd0
Kluzware <robert.kluz@gmail.com>
2022-04-19 13:21:21 +0200

    first commit

---
+++ InterviewConsoleApp2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace InterviewConsoleApp2
+{
+    internal class Program
+    {
+        // Jaki będzie wynik programu?
+        // What will be result of this code?
+        static async Task Main(string[] args)
+        {
+            var collection = new List<string>();
+
+            Task.Run(() =>
+            {
+                try
+                {
+                    Parallel.Invoke(
+                         () => collection.Add("m"),
+                         () => collection.Add("b"),
+                         () => collection.Add("a"),
+                         () => throw new LetsFailSomethingException(),
+                         () => collection.Add("n"),
+                         () => collection.Add("k")
+                    );
+
+                }
+                catch (LetsFailSomethingException)
+                {                  
+                    Console.WriteLine("Hello from LetsFailSomethingException");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Hello from {ex.GetType()}");
+                }
+            });
+
+            foreach (var item in collection)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
